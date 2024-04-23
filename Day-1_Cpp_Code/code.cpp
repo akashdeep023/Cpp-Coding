3ea60e41a1671637cee55e32bfbbc3ff3a58e872
akashdeep023 <ad3500476@gmail.com>
2024-04-23 18:01:04 +0530

    Start C++ && 1st Cpp Code

---
+++ Day-1_Cpp_Code/code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, world!";
+    return 0;
+}
+
+// How to run in terminal ------------------
+// 1st Method ----------------
+// g++ code.css
+// ./a.exe
+
+// 2nd Method ----------------
+// g++ code.css -o code
+// ./code.exe
+
+// 3rd Method ----------------
+// g++ code.css -o code && ./code.exe
